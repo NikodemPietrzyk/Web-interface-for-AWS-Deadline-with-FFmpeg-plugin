a3d41c0c53275656f63e97ebf36dc1b690e63759
NikodemPietrzyk <108872794+NikodemPietrzyk@users.noreply.github.com>
2023-05-31 17:31:16 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+define('DEADLINE_ADRESS', ''); // ip of a deadline client running webservice for REST API
+define('RENDER_NODES', ''); // list of allowed render machines
+define('EMAIL_HOST', ''); 
+define('EMAIL_PORT', '');
+define('EMAIL_DEFAULT_USERNAME', ''); //email for sending mp4
+define('EMAIL_DEFAULT_PASSWORD', ''); //pass to email for sending mp4
+define('EMAIL_SYSTEM_USERNAME', ''); //email for system notifiaction
+define('EMAIL_SYSTEM_PASSWORD', ''); //pass to email for system notifiaction
+define('IP_OR_DNS', ''); // ip adress or dns running the webapp
+define('MAINTAINER', ''); // mail to send error logs from deadline
+define('IT_MAIL', ''); //optional mail to send error logs from deadline
+define('PATH', ''); //define mapping of a network drive on a server running the webapp
+define('NETWORK_DRIVE', ''); 
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+?>
